2206ed7b24f0a4c9f838ec687528e32343a7763a
Victor Jack <67293106+thedon-dev@users.noreply.github.com>
2025-02-09 19:58:57 +0100

    Final Pushes

--- src/app/admin/viewcars/page.js
+++ src/app/admin/viewcars/page.js
@@ -3,6 +3,7 @@
 import axios from "axios";
 import Image from "next/image";
 import React, { useEffect, useState } from "react";
+import { BsTrash } from "react-icons/bs";
 import { FaArrowRight } from "react-icons/fa";
 
 const page = () => {
@@ -26,13 +27,18 @@ const page = () => {
 
     fetchCars();
   }, []);
+
+  const deleteItem = () => {};
   return (
-    <div className="px-[5%]">
+    <div className="">
       <div className="2xl:container mx-auto">
         <h1 className="text-center text-4xl font-bold mt-10">Cars inventory</h1>
-        <div className="w-full mt-14 hide-scroll flex overflow-x-scroll flex-shrink-0 pb-5">
+        <div className="w-full mt-14 hide-scroll flex overflow-x-scroll flex-shrink-0 pb-5 px-10">
           {cars.map((car, index) => (
-            <div className="grid grid-cols-2 w-[30rem] flex-shrink-0">
+            <div
+              key={index}
+              className="grid grid-cols-2 w-[30rem] flex-shrink-0"
+            >
               <Image
                 src={car.image}
                 width={600}
@@ -40,7 +46,7 @@ const page = () => {
                 alt="Tesla-Model"
                 className="h-[55%] object-cover rounded-lg mt-20 z-10"
               ></Image>
-              <div className="bg-white rounded-lg p-5 -translate-x-20">
+              <div className="relative bg-white rounded-lg p-5 -translate-x-20">
                 <h1 className="font-bold text-xl">{car.name}</h1>
                 <h4 className="text-gray-500 text-sm font-semibold mt-1">
                   {car.status}
@@ -58,7 +64,7 @@ const page = () => {
                       Power: <span className="text-black">{car.power}</span>{" "}
                     </p>
                   </div>
-                  <h2 className="mt-7 font-bold text-xl">{car.price}</h2>
+                  <h2 className="mt-7 font-bold text-xl">${car.price}</h2>
                   <div className="mt-5">
                     <a
                       href=""
@@ -67,6 +73,11 @@ const page = () => {
                       Order Now
                       <FaArrowRight />
                     </a>
+                  </div>
+                  <div className="absolute left-0 bottom-0 p-[0.8rem]">
+                    <button onClick={deleteItem} className="">
+                      <BsTrash color="red" />
+                    </button>
                   </div>
                 </div>
               </div>
